bda7e342624936a396bcc49b25a8396c92c6a539
Christopher Magnuson <holymist@hotmail.com>
2018-03-22 17:33:05 -0500

    Change project

--- lab6/DE1_top.vhd
+++ lab6/DE1_top.vhd
@@ -37,13 +37,14 @@ Port(
     
 );
 
-end DE1_top;
+end entity DE1_top;
 
 architecture struct of DE1_top is
 
-	component gen_counter
+	COMPONENT gen_counter
+		
 		generic (
-			wide : positive, -- how many bits is the counter
+			wide : positive; -- how many bits is the counter
 			max  : positive  -- what is the max value of the counter ( modulus )
 			);
 
@@ -56,78 +57,113 @@ architecture struct of DE1_top is
 			count	 :out std_logic_vector( wide-1 downto 0 ); -- count out
 			term	 :out std_logic -- maximum count is reached
 		);
-	END component gen_counter;
+	END COMPONENT gen_counter;
 	
-	component snake_segment_cntrl IS
-		PORT (input : IN unsigned (15 downto 0);
-			output : std_logic_vector( 6 downto 0););
-	END component snake_segment_cntrl;
+	COMPONENT snake_segment_cntrl IS
+	
+		PORT (
+			input : IN unsigned (15 downto 0);
+			output : OUT std_logic_vector( 6 downto 0)
+			);
+			
+	END COMPONENT snake_segment_cntrl;
 
-	component snake_controller
+	COMPONENT snake_controller
+		
 		PORT (
 			-- Declare control inputs 
-			clk, key1, sw1, sw2, sw3, sw4, sw10, count_reset : IN STD_LOGIC;
-
+			reset_a, clk, sw1, sw2, sw3, sw4, sw10 : IN STD_LOGIC;
+			
+			--count_reset : IN STD_LOGIC;
 			-- for time changes; everysecond the snake moves
-			second : IN std_logic;
-			count : in unsigned (25 downto 0);
-			state_out : out unsigned (15 downto 0);
-			done, clk_ena, sclr_n : OUT STD_LOGIC
+			--second : IN std_logic;
+			
+			count: in std_logic;
+			
+			
+			--count : in unsigned (25 downto 0);
+			
+			
+			state_out : out unsigned (15 downto 0)
+			--done, clk_ena, sclr_n : OUT STD_LOGIC
 		);
-	END component snake_controller;
+		
+	END COMPONENT snake_controller;
 
 
 -- signal and component declartions
 -- you will need to create the component declaration for the 7 segment control.
-	signal count unsigned (25 downto 0);
-
-begin
+	--signal count: unsigned (25 downto 0);
+	
+	
+	-- DECLARE ALL COMPONENT OUTPUTS HERE!!!!!
+	
+	
+	signal state_out_before_seg: unsigned(15 downto 0);
+	signal secondPassed : std_logic;
+	
+BEGIN
 
 -- processes, component instantiations, general logic.
 	u1: gen_counter 
-		generic map (wide => 26, max => 50000000);
+		generic map (wide => 26, max => 1000)
 		PORT MAP (
-			clk => clock_50, 
-			aclr_n => start_n, 
-			count_out => count(25 downto 0),
-			reset => count_reset,
-			enable => '1'
+			clk => clock_50,
+			load => '0',
+			data => (others => '0'),
+			--aclr_n => start_n, 
+			--count_out => count(25 downto 0),
+			reset => KEY(0),
+			enable => '1',
+			term => secondPassed
 			);
+			
 	u2: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX0, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX0( 6 downto 0) 
 						);
+						
 	u3: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX1, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX1 ( 6 downto 0)
 						);
+						
 	u4: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX2, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX2 ( 6 downto 0)
 						);
+						
 	u5: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX3, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX3 ( 6 downto 0)
 						);
+						
 	u6: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX4, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX4 ( 6 downto 0)
 						);
+						
 	u7: snake_segment_cntrl PORT MAP (
-						input => state_out(15 downto 0),
-						output => HEX5, 
+						input => state_out_before_seg(15 downto 0),
+						output => HEX5 ( 6 downto 0)
 						);
+						
 	u8: snake_controller PORT MAP (
+		sw1 => SW(0),	-- or 9
+		sw2 => SW(1),	-- or 8
+		sw3 => SW(2),	-- or 7
+		sw4 => SW(3),	-- or 6
+		sw10 => SW(9),	-- or 0
 		clk => clock_50, 
-		reset_a => KEY, 
-		start => start,
-		count => term,
-		input_sel => sel(1 downto 0),
-		shift_sel => shift(1 downto 0),
-		state_out => state_out(2 downto 0),
-		done => done_flag,
-		clk_ena => clk_ena,
-		sclr_n => sclr_n
+		reset_a => KEY(0), -- or 3
+		--start => start,
+		count => secondPassed,
+		--input_sel => sel(1 downto 0),
+		--shift_sel => shift(1 downto 0),
+		state_out => state_out_before_seg--(2 downto 0)
+		--done => done_flag,
+		--clk_ena => clk_ena,
+		--sclr_n => sclr_n
 		);
 				
 end;
--- lab6/snake_controller.vhd
+++ lab6/snake_controller.vhd
@@ -2,187 +2,173 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 -- Begin entity declaration for "control"
-ENTITY snake_control IS
+ENTITY snake_controller IS
 	-- Begin port declaration
 	PORT (
 		-- Declare control inputs 
-		clk, key1, sw1, sw2, sw3, sw4, sw10 : IN STD_LOGIC;
+		clk, reset_a : IN STD_LOGIC; -- system clock, custom reset signal(KEY1 Press)
+		sw1, sw2, sw3, sw4, sw10 : IN STD_LOGIC; --Switch values from the switches vector
 
-		-- for time changes; everysecond the snake moves
-		second : IN std_logic;
-		count : in unsigned (25 downto 0);
-		state_out : out unsigned (15 downto 0);
-		count_reset : out std_logic;
+		-- count signal if the counter has reached 50 mil
+		count : in std_logic;
+		
+		
+		--count : in unsigned (25 downto 0);
+		
+		
+		-- Values of what the 6 segments should display
+		state_out : out unsigned (15 downto 0)
+		
+		
+		--count_reset : out std_logic
 
-		-- Declare output control signals  AKA the segments for the seven seg display(6 of these)
 	);
-END ENTITY snake_control;
---  Begin architecture 
-ARCHITECTURE logic OF snake_control IS
-	TYPE state_type IS (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15);
-	-- maybe err state
-	-- Declare two signals named "head_state" and "next_state" to be of enumerated type
-	SIGNAL head_state: state_type;
-	SIGNAL next_state: state_type;
-	signal size: unsigned (15 downto 0);
-	signal shift_count: integer;
-BEGIN
-	--  rising edge transitions; Use asynchronous clear control
+	
+	
+END ENTITY snake_controller;
+	--  Begin architecture 
+	
+	ARCHITECTURE logic OF snake_controller IS
+		TYPE state_type IS (s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15);
 
-	-- set the next state aka move the snake around the clock.
-	PROCESS (clk, reset_a)
-	begin 
-		if reset_a = '1' then
-			head_state <= s1;
-		elsif rising_Edge(clk) then
-			head_state <= next_state;
-		end if;
-	END PROCESS;
+		-- Declare two signals named "head_state" and "next_state" to be of enumerated type
+		SIGNAL head_state: state_type;
+		SIGNAL next_state: state_type;
+		signal size: unsigned (15 downto 0);
+		signal shift_count: integer;
+		
+	BEGIN
+		--  rising edge transitions; Use asynchronous clear control
+		-- set the next state aka move the snake around the clock.
+		
+		-- remove clk and use count instead??
+		
+		PROCESS (clk, reset_a)
+		begin 
+			if reset_a = '0' then
+				head_state <= s1;
+			elsif rising_Edge(clk) then
+				head_state <= next_state;
+			end if;
+		END PROCESS;
 
 	-- Figure out the next state for the head based on if they have been swapped
 	-- and if the second has passed.
-	PROCESS (count)
+	PROCESS (head_state, count, sw10)
 	BEGIN
-		CASE head_state IS
-			WHEN s1 =>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s16;
-					ELSE
-						next_state <= s2;
-					END IF;
-				END IF;
+		IF count = '1' THEN
+			CASE head_state IS
+				WHEN s0 =>
+						IF sw10 = '1' THEN
+							next_state <= s15;
+						ELSE
+							next_state <= s1;
+						END IF;
 
-			WHEN s2=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s1;
-					ELSE
-						next_state <= s3;
-					END IF;
-				END IF;
+				WHEN s1=>
+						IF sw10 = '1' THEN
+							next_state <= s0;
+						ELSE
+							next_state <= s2;
+						END IF;
 
-			WHEN s3=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s2;
-					ELSE
-						next_state <= s4;
-					END IF;
-				END IF;
+				WHEN s2=>
+						IF sw10 = '1' THEN
+							next_state <= s1;
+						ELSE
+							next_state <= s3;
+						END IF;
 
-			WHEN s4=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s3;
-					ELSE
-						next_state <= s5;
-					END IF;
-				END IF;
-			WHEN s5=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s4;
-					ELSE
-						next_state <= s6;
-					END IF;
-				END IF;
-			WHEN s6=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s5;
-					ELSE
-						next_state <= s7;
-					END IF;
-				END IF;
-			WHEN s7=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s6;
-					ELSE
-						next_state <= s8;
-					END IF;
-				END IF;
-			WHEN s8=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s7;
-					ELSE
-						next_state <= s9;
-					END IF;
-				END IF;
-			WHEN s9=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s8;
-					ELSE
-						next_state <= s10;
-					END IF;
-				END IF;
-			WHEN s10=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s9;
-					ELSE
-						next_state <= s11;
-					END IF;
-				END IF;
-			WHEN s11=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s10;
-					ELSE
-						next_state <= s12;
-					END IF;
-				END IF;
-			WHEN s12=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s11;
-					ELSE
-						next_state <= s13;
-					END IF;
-				END IF;
-			WHEN s13=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s12;
-					ELSE
-						next_state <= s14;
-					END IF;
-				END IF;
-			WHEN s14=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s13;
-					ELSE
-						next_state <= s15;
-					END IF;
-				END IF;
-			WHEN s15=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s14;
-					ELSE
-						next_state <= s16;
-					END IF;
-				END IF;
-			WHEN s16=>
-				IF count = '1' THEN
-					IF sw10 = '1' THEN
-						next_state <= s15;
-					ELSE
-						next_state <= s1;
-					END IF;
-				END IF;
-
-		END CASE;
-		count_reset <= '1';
+				WHEN s3=>
+						IF sw10 = '1' THEN
+							next_state <= s2;
+						ELSE
+							next_state <= s4;
+						END IF;
+				WHEN s4=>
+						IF sw10 = '1' THEN
+							next_state <= s3;
+						ELSE
+							next_state <= s5;
+						END IF;
+				WHEN s5=>
+						IF sw10 = '1' THEN
+							next_state <= s4;
+						ELSE
+							next_state <= s6;
+						END IF;
+				WHEN s6=>
+						IF sw10 = '1' THEN
+							next_state <= s5;
+						ELSE
+							next_state <= s7;
+						END IF;
+				WHEN s7=>
+						IF sw10 = '1' THEN
+							next_state <= s6;
+						ELSE
+							next_state <= s8;
+						END IF;
+				WHEN s8=>
+						IF sw10 = '1' THEN
+							next_state <= s7;
+						ELSE
+							next_state <= s9;
+						END IF;
+				WHEN s9=>
+						IF sw10 = '1' THEN
+							next_state <= s8;
+						ELSE
+							next_state <= s10;
+						END IF;
+				WHEN s10=>
+						IF sw10 = '1' THEN
+							next_state <= s9;
+						ELSE
+							next_state <= s11;
+						END IF;
+				WHEN s11=>
+						IF sw10 = '1' THEN
+							next_state <= s10;
+						ELSE
+							next_state <= s12;
+						END IF;
+				WHEN s12=>
+						IF sw10 = '1' THEN
+							next_state <= s11;
+						ELSE
+							next_state <= s13;
+						END IF;
+				WHEN s13=>
+						IF sw10 = '1' THEN
+							next_state <= s12;
+						ELSE
+							next_state <= s14;
+						END IF;
+				WHEN s14=>
+						IF sw10 = '1' THEN
+							next_state <= s13;
+						ELSE
+							next_state <= s15;
+						END IF;
+				WHEN s15=>
+						IF sw10 = '1' THEN
+							next_state <= s14;
+						ELSE
+							next_state <= s0;
+						END IF;
+				WHEN others =>
+					next_state <= s0;
+			END CASE;
+		ELSE
+			next_state <= head_state;
+		END IF;
+		--count_reset <= '1';
 	-- End process
 	END PROCESS;
 
 	process (sw1, sw2, sw3, sw4)
-		variable tempsize unsigned (16 downto 0 ) := "0000000000000000";
+		variable tempsize : unsigned (15 downto 0 ) := "0000000000000000";
 	begin
 		if sw4 = '0' and sw3 = '0' and sw2 = '0' and sw1 = '1' then
 			tempsize := "1000000000000000";
@@ -224,7 +210,7 @@ BEGIN
 	moore: PROCESS(head_state)
 	BEGIN
 		-- Initialize state_out to default values so case only covers when they change
-		state_out <= (others => '0');
+		--state_out <= (others => '0');
 		CASE head_state IS
 			WHEN s1 =>
 				shift_count <= 15;
